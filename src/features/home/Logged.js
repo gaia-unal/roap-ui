9fce80c1b8b937ad3de0cad7eb39c1fbdc845165
dsvalenciah <dsvalenciah@unal.edu.co>
2018-03-20 13:49:08 -0500

    Change icon

--- src/features/home/Logged.js
+++ src/features/home/Logged.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import ActionAccountCircle from 'material-ui/svg-icons/action/account-circle';
 
 export default class Logged extends Component {
   static propTypes = {
@@ -13,8 +13,8 @@ export default class Logged extends Component {
     return (
       <IconMenu
         iconButtonElement={
-          <IconButton>
-            <MoreVertIcon />
+          <IconButton iconStyle={{ marginRight: '20px' }}>
+            <ActionAccountCircle />
           </IconButton>
         }
         targetOrigin={{ horizontal: 'right', vertical: 'top' }}
