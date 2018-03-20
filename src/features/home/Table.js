a6bbb79a62a09adf1407584f62780579c4a983fa
dsvalenciah <dsvalenciah@unal.edu.co>
2018-03-20 13:49:35 -0500

    Change table style

--- src/features/home/Table.js
+++ src/features/home/Table.js
@@ -10,24 +10,28 @@ import request from 'superagent';
 import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
 import {List, ListItem} from 'material-ui/List';
-import ActionInfo from 'material-ui/svg-icons/action/info';
+import ActionDescription from 'material-ui/svg-icons/action/description';
+import SocialVeryDissatisfied from 'material-ui/svg-icons/social/sentiment-very-dissatisfied';
+import SocialDissatisfied from 'material-ui/svg-icons/social/sentiment-dissatisfied';
+import SocialNeutral from 'material-ui/svg-icons/social/sentiment-neutral';
+import SocialSatisfied from 'material-ui/svg-icons/social/sentiment-satisfied';
+import SocialVerySatisfied from 'material-ui/svg-icons/social/sentiment-very-satisfied';
+import {
+  darkBlack,
+  blueGrey200,
+  blueGrey500,
+  red900,
+  orange900,
+  yellow900,
+  lightGreen900,
+  green900,
+} from 'material-ui/styles/colors';
+import Divider from 'material-ui/Divider';
+import LinearProgress from 'material-ui/LinearProgress';
+import Dialog from 'material-ui/Dialog';
+import LearningObjectScoreService from './services/LearningObjectScore';
+import IconButton from 'material-ui/IconButton';
 
-
-class LearningObjectService {
-  constructor() {
-    this.url = 'http://localhost/back/object';
-  }
-
-  get(then) {
-    request
-      .get(this.url)
-      .query({ offset: 0, count: 10 })
-      .end((err, res) => {
-        if (!err) then(res);
-        else console.error(err);
-      });
-  }
-}
 
 export class LearningObjectTable extends Component {
   static propTypes = {
@@ -35,44 +39,158 @@ export class LearningObjectTable extends Component {
     actions: PropTypes.object.isRequired,
   };
 
-  learningObjectService = new LearningObjectService();
+  insertSvgIconIntoIconButton(tooltip, color, SvgIcon) {
+    return <IconButton
+      tooltip={tooltip}
+      tooltipPosition='top-right'
+      style={{padding: '4px', width: '60', height: '60'}}
+      iconStyle={{ margin: '-2px', color: color }}
+    >
+      {SvgIcon}
+    </IconButton>
+  }
+
+  getRankingIcon(ranking, tooltip) {
+    switch(ranking) {
+      case 1: 
+        return this.insertSvgIconIntoIconButton(tooltip, red900,
+          <SocialVeryDissatisfied hoverColor={blueGrey200} />
+        );
+        break;
+      case 2: 
+        return this.insertSvgIconIntoIconButton(tooltip, orange900,
+          <SocialDissatisfied hoverColor={blueGrey200} />
+        );
+        break;
+      case 3: 
+        return this.insertSvgIconIntoIconButton(tooltip, yellow900,
+          <SocialNeutral hoverColor={blueGrey200} />
+        );
+        break;
+      case 4: 
+        return this.insertSvgIconIntoIconButton(tooltip, lightGreen900,
+          <SocialSatisfied hoverColor={blueGrey200} />
+        );
+        break;
+      case 5: 
+        return this.insertSvgIconIntoIconButton(tooltip, green900,
+          <SocialVerySatisfied hoverColor={blueGrey200} />
+        );
+        break;
+    }
+  }
 
   state = {
-    learningObjectList: []
+    learningObjectList: [],
+    learningObjectScore: [],
+    showDescription: false,
+    showMetadata: false,
+    learningObjectDescription: '',
+    learningObjectTitle: '',
+    learningObjectMetadata: ''
   };
 
-  componentDidMount() {
-    this.learningObjectService.get(res => this.setState({
-      learningObjectList: res.body
-    }));
+  learningObjectScoreService = new LearningObjectScoreService();
+
+  getLearningObjectScore(_id) {
+    this.learningObjectScoreService.get(
+      _id,
+      res => {this.setState( {
+        learningObjectScore: [...this.state.learningObjectScore, res.body]
+      })}
+    )
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({ learningObjectList: nextProps.learningObjectList });
+    this.setState({ learningObjectScore: [] }, () => {
+      this.setState(
+        { learningObjectList: nextProps.learningObjectList },
+        () => { _.map(
+          this.state.learningObjectList, (lo) => {this.getLearningObjectScore(lo._id)}
+        ) }
+      );
+    });
   }
 
   render() {
     return (
       <div className="home-table">
+        <Dialog
+          title="Description"
+          open={this.state.showDescription}
+          onRequestClose={() => { this.setState({ showDescription: false, showMetadata: false }); }}
+        >
+          <span style={{color: darkBlack}}>{this.state.learningObjectTitle}</span><br />
+          {this.state.learningObjectDescription}
+        </Dialog>
+        <Dialog
+          autoScrollBodyContent={true}
+          title="Metadata"
+          open={this.state.showMetadata && !this.state.showDescription}
+          onRequestClose={() => { this.setState({ showMetadata: false }); }}
+        >
+          {this.state.learningObjectMetadata}
+        </Dialog>
         <center>
-          <Paper zDepth={4} style={{ width: '50%', height: '70%', padding: 25, minWidth: '480px' }}>
+          <Paper zDepth={4} style={{ width: '65%', height: '70%', padding: 10, minWidth: '480px' }}>
             <List>
               {_.map(this.state.learningObjectList, (lo, id) => (
-                <ListItem
-                  key={id}
-                  primaryText={lo.metadata.general.title}
-                  secondaryText={lo.created}
-                  leftAvatar={
-                    <Avatar size={50} style={{ fontSize: '20px' }}>
-                    {String(lo.metadata.technical.format).substring(0, 3)}
-                    </Avatar>
-                  }
-                  rightIcon={
-                    <ActionInfo
-                      onClick={() => { console.log('holi'); }}
-                    />
-                  }
-                />
+                <div key={id}>
+                  <ListItem
+                    onClick={() => {this.setState( { 
+                      showMetadata: !this.state.showMetadata,
+                      learningObjectMetadata: (
+                        <pre> {JSON.stringify(lo.metadata, undefined, 4)} </pre>
+                      )
+                    } )}}
+                    style={{textAlign: 'left', padding: '15px'}}
+                    primaryText={lo.metadata.general.title}
+                    secondaryText={
+                      <p>
+                        <span style={{color: darkBlack}}>{lo.created}</span>{ ' -- ' }
+                        {lo.metadata.general.description}
+                      </p>
+                    }
+                    secondaryTextLines={2}
+                    leftAvatar={
+                      <Avatar size={50}
+                        style={{ fontSize: '20px', marginLeft: '-20px'}}
+                      >
+                        {String(lo.category).substring(0, 4)}
+                      </Avatar>
+                    }
+                    rightIcon={
+                      <div>
+                        {this.state.learningObjectScore[id] && (
+                          <div>
+                            {this.getRankingIcon(
+                              Math.round(this.state.learningObjectScore[id].creator!=undefined?this.state.learningObjectScore[id].creator:1),
+                              'Creator score: ' + (this.state.learningObjectScore[id].creator!=undefined?this.state.learningObjectScore[id].creator:1))}
+                            {this.getRankingIcon(
+                              Math.round(this.state.learningObjectScore[id].expert!=undefined?this.state.learningObjectScore[id].expert:1),
+                              'Expert score: ' + (this.state.learningObjectScore[id].expert!=undefined?this.state.learningObjectScore[id].expert:1))}
+                          </div>
+                        )}
+                      <IconButton
+                        tooltip={'View full description'}
+                        tooltipPosition='top-right'
+                        style={{padding: '4px', width: '60', height: '60'}}
+                        iconStyle={{ margin: '-2px', color: blueGrey500 }}
+                      >
+                        <ActionDescription
+                          onClick={() => { this.setState({
+                            showDescription: !this.state.showDescription,
+                            learningObjectDescription: lo.metadata.general.description,
+                            learningObjectTitle: lo.metadata.general.title,
+                          }); }}
+                          hoverColor={blueGrey200}
+                        />
+                      </IconButton>
+                      </div>
+                    }
+                  />
+                  <Divider inset={true} />
+                </div>
               ))}
             </List>
           </Paper>
