eb0f399495f2bed3561d0e52fdf57f2103b4748a
Orlando Hernandez <oaraz02@gmail.com>
2019-06-06 23:03:31 -0500

    add logo in navbar

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { AppBar, UserMenu, MenuItemLink, translate } from 'react-admin';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { withStyles } from "@material-ui/core/styles";
 
 const CustomUserMenu = translate(({ translate, ...props }) => (
   <UserMenu {...props}>
     <MenuItemLink to="/configuration" primaryText={translate('tabs_name.configuration')} leftIcon={<SettingsIcon />} />
   </UserMenu>
 ));
-
-const CustomAppBar = props => <AppBar {...props} userMenu={<CustomUserMenu />} />;
+const styles = {
+  spacer: {
+    flex: 1
+  }
+};
+const CustomAppBar = withStyles(styles)(({ classes, ...props }) => (
+  <AppBar
+    {...props}
+    userMenu={<CustomUserMenu />}
+  >
+    <h1>ROAp</h1>
+    <span className={classes.spacer} />
+  </AppBar>
+));
 
 export default CustomAppBar;
